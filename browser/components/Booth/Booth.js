b3b177f82b7c0fe478f35bd9569cac09d410a22d
Isaac Ibiapina <isaac1@mail.usf.edu>
2017-05-05 14:21:08 -0400

    create SongBox component

--- browser/components/Booth/Booth.js
+++ browser/components/Booth/Booth.js
@@ -2,8 +2,17 @@ import React from 'react';
 import 'aframe';
 import { Scene, Entity } from 'aframe-react';
 import daydream from 'aframe-daydream-controller-component'
+import SongBox from './SongBox'
 
-const Booth = () => (
+export default class extends React.Component {
+
+  constructor(){
+    super()
+  }
+
+
+  render(){
+    return (
     <Scene>
       <Entity id="remote" daydream raycaster="objects: .selectable">
       <Entity primitive="a-cone" color="cyan" position="0 0 -2" rotation="-90 0 0" radius-bottom="0.005" radius-top="0.001" height="4"/>
@@ -12,18 +21,11 @@ const Booth = () => (
 
       <Entity primitive="a-sky" color='#2d2c2c'/>
 
+      <SongBox text="Howdyy" textColor="black" boxColor="white" position="0 2 -2" width="1.50" height="0.5"/>
 
-      <Entity primitive="a-plane">
-        <Entity text={{value: 'Text Here', color:'purple', width:'10',height:'10' }} id="song" position="0 2 -2" class="selectable"/>
-      </Entity>
+      <SongBox text="Howdyy2" textColor="black" boxColor="white" position="0 1 -2" width="1.50" height="0.5"/>
 
-      <Entity text={{value: 'Hello, WebVR!'}} />
-      <Entity primitive="a-plane">
-      </Entity>
-      <div><p>ewffwfwefwefw</p></div> //not sure why but the Goggles icon is too high up
-      <div><p>ewffwfwefwefw</p></div> //these two divs push it down
+
     </Scene>
-);
-
-
-export default Booth;
+)}
+}
