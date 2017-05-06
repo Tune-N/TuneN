05d63d46879307700212b3dc9b89d58bc89297f8
MarcosRS <MarcosRS16@gmail.com>
2017-05-06 15:08:16 -0400

    update env variables

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ const {resolve} = require('path')
 const passport = require('passport')
 const startDb = require('../db');
 
-app.listen(3000, function () {
-  console.log("Your server is listening on port 3000");
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function () {
+  	console.log('Node app is running on port', app.get('port'));
 
   //Promise
     startDb
