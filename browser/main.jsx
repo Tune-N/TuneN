9a9b1db5d010dd70a8c67850cecb03c53f716f04
Samir Awuapara <samirawuapara@Samirs-MacBook-Air.local>
2017-05-08 07:27:29 -0400

    Update Description on package.json Clean eslint errors.

--- browser/main.jsx
+++ browser/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store.js';
+import store from './store';
 import { BrowserRouter, Link, Route, browserHistory, IndexRedirect } from 'react-router-dom';
 
-import App from './app.js';
-import '../public/stylesheets/index.scss'
-import '../public/stylesheets/nav.scss'
+import App from './app';
+import '../public/stylesheets/index.scss';
+import '../public/stylesheets/nav.scss';
 
 ReactDOM.render(
   <Provider store={ store }>
