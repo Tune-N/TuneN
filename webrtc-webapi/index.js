862927f632b57d48f1a6ccdc53beebb7b8bcb2c5
MarcosRS <MarcosRS16@gmail.com>
2017-05-11 21:50:53 -0400

    update: webrtc channel

--- webrtc-webapi/index.js
+++ webrtc-webapi/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const path = require('path');
 const app = new express();
+const stream = require('youtube-audio-stream');
+const url = 'https://www.youtube.com/watch?v=FHkMT1Vxi5I'
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './public')));
 
+app.use('/client', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/client_view.html'));
+});
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    // stream(url).pipe(res)
+
+    // console.log()
+
+    // stream(url).pipe(res.sendFile(path.join(__dirname + 'public/index.html')))
 });
 
 app.listen(port, function(){
