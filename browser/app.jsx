eb1f95ba5b91ba406bca31993acee0762245f15f
Samir Awuapara <samir8787@gmail.com>
2017-05-14 23:59:14 -0400

    Create component for Homepage.

--- browser/app.jsx
+++ browser/app.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import Homepage from './Homepage/Homepage.jsx'
 import Navbar from './components/Navbar';
 import djBoothContainer from './components/djBooth/containers/djBoothContainer'
 
 const App = () => (
   <div id="main" className="container-fluid">
-    <Route exact path="/" component={Navbar} />
-    <Route path="/vr" component={djBoothContainer} />
+    <Route exact path="/" component={Homepage} />
+    <Route path="/signup" component={Homepage} />
+    <Route path="/login" component={Homepage} />
+    <Route path="/room" component={djBoothContainer} />
+    <Route path="/dj" component={djBoothContainer} />
+
   </div>
 );
 
