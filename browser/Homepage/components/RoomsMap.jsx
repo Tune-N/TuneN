9d33648eb0db0679b8c828ab22106578326612e6
Samir Awuapara <samir8787@gmail.com>
2017-05-15 22:50:45 -0400

    Update map to black theme.

--- browser/Homepage/components/RoomsMap.jsx
+++ browser/Homepage/components/RoomsMap.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { withGoogleMap, GoogleMap, Marker, SearchBox } from "react-google-maps";
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
+
+import mapStyles from '../../../public/stylesheets/mapStyles.json';
 
 // #TODO: remove duplicate rooms_map ids
-const RoomsMap = withGoogleMap(props => (
-
-  <GoogleMap id="rooms_map"
+const RoomsMap = withGoogleMap(() => (
+  <GoogleMap
+    id="rooms_map"
     defaultZoom={8}
     defaultCenter={{ lat: 40.690218, lng: -74.0962687 }}
-  >
-  </GoogleMap>
+    defaultOptions={{ styles: mapStyles }}
+  />
 ));
 
 export default RoomsMap;
