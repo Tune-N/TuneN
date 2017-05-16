1e68d7c990e96975c9ddce192a24c09d9a247bf8
Isaac Ibiapina <isaac1@mail.usf.edu>
2017-05-16 14:22:27 -0400

    modify Navbar for ternary and manterial UI

--- browser/components/Navbar.jsx
+++ browser/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { AppBar } from 'material-ui';
 import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton';
 
+
 import { logout } from '../reducers/auth/actions-creators';
 
 // Title Styles
@@ -20,19 +21,30 @@ const styles = {
 
 const Navbar = props => {
   const { user, logout} = props;
+
+  // const items = {user ? {<Link to="dj"><FlatButton key={1} label="DJ Live"/></Link>
+  //   <FlatButton key={2} label="Sign out" onClick={()=>logout()} />}
+  //
+  // :   {<Link to="/login"><FlatButton key={4} label="Login" /></Link>}
+
+  const Loggedin = () => {
+    return (<div>
+      <Link to="dj"><FlatButton label="DJ Live"/></Link>
+      < FlatButton key={2} label="Sign out" onClick={()=>logout()} />
+    </div>)
+  }
+
+  const NotLoggedIn = () => (
+    <Link to="/login"><FlatButton label="Login" labelStyle={{fontSize:15}}/></Link>
+  )
+
   return (
     <div>
       <AppBar
         title={<span style={styles.title}>TuneN</span>}
+        style={{ backgroundColor: '#021019', color: '#FFFFFF', paddingTop:8}}
         iconElementLeft={<IconButton />}
-        iconElementRight={user ?
-          <FlatButton
-            style={{ color: '#FFFFFF' }}
-            label="Signout"
-            onClick={() => logout()}
-          /> :
-          <Link to="/login"><FlatButton label="Login" /></Link>}
-        style={{ backgroundColor: '#021019', color: '#FFFFFF' }}
+      iconElementRight={ user ? <Loggedin/>: <NotLoggedIn/>}
       />
     </div>
   );
@@ -47,3 +59,21 @@ const mapDispatchToProps = {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+
+{/*<AppBar*/}
+  {/*title={<span style={styles.title}>TuneN</span>}*/}
+  {/*style={{ backgroundColor: '#021019', color: '#FFFFFF' }}*/}
+  {/*iconElementLeft={<IconButton />}*/}
+  {/*iconElementRight={<IconMenu*/}
+    {/*{...props}*/}
+    {/*iconButtonElement={*/}
+      {/*<IconButton ><MoreVertIcon style={iconStyles} color={blue500}/></IconButton>*/}
+    {/*}*/}
+    {/*targetOrigin={{horizontal: 'right', vertical: 'top'}}*/}
+    {/*anchorOrigin={{horizontal: 'right', vertical: 'top'}}*/}
+  {/*>*/}
+    {/*<Link to="/dj"><MenuItem primaryText="DJ Live" /></Link>*/}
+    {/*<MenuItem primaryText="Sign out" onClick={()=> logout()}/>*/}
+
+  {/*</IconMenu>}*/}
+{/*/>*/}
