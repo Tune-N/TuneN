a20c776bde7856d57007b43dccc0f227955efa90
Samir Awuapara <samir8787@gmail.com>
2017-05-16 21:58:33 -0400

    old rename

--- browser/DJ/containers/djBoothContainer.js
+++ browser/DJ/containers/djBoothContainer.js
@@ -1,18 +1,17 @@
 import { connect } from 'react-redux';
 
-import { goLive, endSession, setLocation } from '../../reducers/djBooth/action-creators';
+import { goLive, endSession } from '../../reducers/djBooth/action-creators';
 
 import djBooth from '../components/DJBooth.jsx';
 
 const mapStateToProps = state => ({
-  username: state.auth.username,
+  user: state.auth,
   djBooth: state.djBooth,
 });
 
 const mapDispatchToProps = {
-  setLocation,
   goLive,
-  endSession,
+  endSession
 };
 
 const djBoothContainer = connect(mapStateToProps, mapDispatchToProps)(djBooth);
