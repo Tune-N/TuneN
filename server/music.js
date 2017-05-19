3dcf7d71a025b24c1d50a8bee8b5ac0f7eb41aa8
MarcosRS <MarcosRS16@gmail.com>
2017-05-18 22:37:02 -0400

    and another one! ytdl-code

--- server/music.js
+++ server/music.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const router = require('express').Router();
-const stream = require('youtube-audio-stream');
+// const stream = require('youtube-audio-stream');
+const ytdl = require('ytdl-core');
 
 router.get('/youtube/mp3/:videoId',function (req, res, next) {	
-  stream(`https://www.youtube.com/watch?v=${req.params.videoId}`).pipe(res)
+  // stream(`https://www.youtube.com/watch?v=${req.params.videoId}`).pipe(res)
+  ytdl(`https://www.youtube.com/watch?v=${req.params.videoId}`).pipe(res)
 });
 
 
